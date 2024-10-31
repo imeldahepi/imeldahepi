cc21b91482d4b0ad8aadf208c4fada0fc6f927e7
imeldahepi <151838060+imeldahepi@users.noreply.github.com>
2024-10-31 15:11:48 +0700

    Add files via upload

---
+++ android/app/src/main/kotlin/com/biodatacarissa/biodatacarissa/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.biodatacarissa.biodatacarissa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/models/api.dart
@@ -0,0 +1,7 @@
+class BaseUrl{
+  static String tambah = "http://192.168.137.186/biodatacarissa/create.php";
+  static String lihat = "http://192.168.137.186/biodatacarissa/details.php";
+  static String edit = "http://192.168.137.186/biodatacarissa/update.php";
+  static String hapus = "http://192.168.137.186/biodatacarissa/delete.php";
+  static String data = "http://192.168.137.186/biodatacarissa/list.php";
+}
